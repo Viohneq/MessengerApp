ede37442922c5f280da8fedbbe9f516b4c3c344d
Yuriy <yuriy.egorov@team.bumble.com>
2022-06-19 22:55:33 +0300

    Initialazed project

---
+++ MessengerApp/MessengerApp/MessengerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MessengerAppApp.swift
+//  MessengerApp
+//
+//  Created by Yuriy Egorov on 19/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct MessengerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
